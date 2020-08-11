e0718d76207ac16787bba4dd5b7d7e50b5ea792a
Jamie Schrifrin <jamieschifrin@hotmail.com>
2020-08-11 15:18:41 -0400

    Updates

---
+++ precache-manifest.449d2786f28f0faa994dcde1073c04f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5692c747c954e63c38f53a858f7d0f7d",
+    "url": "/toDo/index.html"
+  },
+  {
+    "revision": "5f93e07c43382e56ace3",
+    "url": "/toDo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed1012d95769e620f308",
+    "url": "/toDo/static/js/2.b8f6d5ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDo/static/js/2.b8f6d5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f93e07c43382e56ace3",
+    "url": "/toDo/static/js/main.5c2741a4.chunk.js"
+  },
+  {
+    "revision": "62750a06c6f0268bab2f",
+    "url": "/toDo/static/js/runtime-main.f5a8938e.js"
+  }
+]);
